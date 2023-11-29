76e0712ba74572ab615e938c2a7d1e17a78952a2
shayangiri49 <sawon498@gmail.com>
2023-11-29 14:37:47 +0530

    push

---
+++ src/components/SharedMessages.js
@@ -0,0 +1,116 @@
+import {
+  Box,
+  Grid,
+  IconButton,
+  Stack,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { UpdateSidebarType } from "../redux/slices/app";
+import { CaretLeft } from "phosphor-react";
+import { faker } from "@faker-js/faker";
+
+const SharedMessages = () => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: 320, height: "100vh" }}>
+      <Stack sx={{ height: "100%" }}>
+        {/* Header */}
+        <Box
+          sx={{
+            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+            width: "100%",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+          }}
+        >
+          <Stack
+            sx={{ height: "100%", p: 2 }}
+            direction="row"
+            alignItems={"center"}
+            spacing={3}
+          >
+            <IconButton
+              onClick={() => {
+                dispatch(UpdateSidebarType("CONTACT"));
+              }}
+            >
+              <CaretLeft />
+            </IconButton>
+            <Typography variant="subtitle2">Shared Messages</Typography>
+          </Stack>
+        </Box>
+
+        <Tabs
+          sx={{ px: 2, pt: 2 }}
+          value={value}
+          onChange={handleChange}
+          centered
+        >
+          <Tab label="Media" />
+          <Tab label="Links" />
+          <Tab label="Docs" />
+        </Tabs>
+
+        {/* Body */}
+        <Stack
+          sx={{
+            height: "100%",
+            position: "relative",
+            flexGrow: 1,
+            overflowY: "scroll",
+            "&::-webkit-scrollbar": { display: "none" },
+          }}
+          p={3}
+          spacing={3}
+        >
+          {(()  =>{
+            switch (value) {
+              case 0:
+                //Images
+                return (
+                  <Grid container spacing={2}>
+                    {[0, 1, 2, 3, 4, 5, 6].map((el) => {
+                      return (
+                        <Grid item xs={4}>
+                          <img
+                            src={faker.image.avatar()}
+                            alt={faker.name.fullName()}
+                          />
+                        </Grid>
+                      );
+                    })}
+                  </Grid>
+                );
+
+              case 1:
+                //Links
+                break;
+              case 2:
+                //Docs
+                break;
+
+              default:
+                break;
+            }
+          })()}
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default SharedMessages;
